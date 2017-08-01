a5af9441d093f824cefba30b5bb960f167d2872a
Katy DiPasquale <katy.dipasquale@libertymutual.com>
2017-08-01 14:49:10 -0400

    changes to package.json

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
+
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 4200);
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-app.use(forceSSL());
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// app.use(forceSSL());
